f6b36453294c0b22a0297cd4ddf801d9e0ce17ba
Kearney <kearneyn@brynmawrschool.org>
2018-11-06 10:45:27 -0500

    first commit

---
+++ TeleOpSkeleton.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Sample TeleOp", group="Iterative Opmode")
+public class sampleTeleOp extends OpMode {
+
+    /*
+     * Declare Motors, Servos, etc. here
+     */
+
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     * 
+     * E.g. Initialization of Motors, Servos, etc. via hardware map
+     */
+    @Override
+    public void init() {
+        
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     *
+     */
+    @Override
+    public void init_loop() {
+        
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     *
+     * E.g. anything that needs to be reset, moved, prepped for TeleOp
+     */
+    @Override
+    public void start(){
+        
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     *
+     * I.e. Your main control loop
+     */
+    @Override
+    public void loop() {
+        
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     *
+     * E.g. any retracting, latching, locking to be done at end game.  Power down motors to stop.
+     */
+    @Override
+    public void stop() {
+        
+    }
+}
